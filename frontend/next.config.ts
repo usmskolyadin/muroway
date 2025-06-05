da9e867f7495e49fcbaee130b857a9ed6ccd824f
usmskolyadin <koladinmaksim246@gmail.com>
2025-06-05 20:41:38 +0300

    some 5

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     productionBrowserSourceMaps: false,
+    experimental: {
+        allowedDevOrigins: ['https://muroway.ru'],
+    },
 }
 
 module.exports = nextConfig
