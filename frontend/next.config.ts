123ec15fe219b02c7f9e944a3d7f1c1c5eb5aa4f
usmskolyadin <koladinmaksim246@gmail.com>
2025-06-25 12:47:59 +0300

    fix images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,15 @@ const nextConfig = {
     },
     images: {
         domains: ['714e50aa-8d8643a0-1bd4-46a8-a088-e77845dd8cbb.s3.twcstorage.ru'],
-      },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '714e50aa-8d8643a0-1bd4-46a8-a088-e77845dd8cbb.s3.twcstorage.ru',
+                pathname: '/**',
+            },
+        ],  
+    },
+      
 }
 
 module.exports = nextConfig
